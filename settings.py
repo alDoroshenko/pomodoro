6892e79cae5862db2f0a54d53c7de590a4616aed
aldoroshenko <aldoroshenko@neoflex.ru>
2025-08-14 18:03:13 +0400

    add settings

--- settings.py
+++ settings.py
@@ -1,5 +1,11 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    GOOGLE_TOKEN_ID: str = "zaglushka"
-    sqlite_db_name: str = "SQLite/pomodoro.sqlite"
+    DB_HOST: str = 'localhost'
+    DB_PORT: int = 5435
+    DB_USER: str = 'postgres'
+    DB_PASSWORD: str = 'postgres'
+    DB_NAME: str = 'pomodoro'
+    CACHE_HOST: str = 'localhost'
+    CACHE_PORT: int = 6380
+    CACHE_DB: int = 0
