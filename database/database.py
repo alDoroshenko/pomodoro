d109fe2c61f24a6b539b65b75030fe4c5985d5a2
aldoroshenko <aldoroshenko@neoflex.ru>
2025-08-15 13:50:37 +0400

    feature: User, Logi. create and login user

--- database/database.py
+++ database/database.py
@@ -1,14 +1,15 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from settings import Settings
+from typing import Any
 
-settings = Settings()
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 
-engine = create_engine(settings.db_url)
-Session = sessionmaker(engine)
 
-def get_db_session()-> Session:
-    return Session
+class Base (DeclarativeBase):
+    id: Any
+    __name__ = str
 
-def new_func():
-    pass
+    __allow_unmapped__= True
+
+    @declared_attr
+    def __tablename__(self)-> str:
+        return self.__name__.lower()
+
